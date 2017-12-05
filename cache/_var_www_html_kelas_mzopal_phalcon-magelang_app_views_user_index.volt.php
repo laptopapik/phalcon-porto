d38ecfcaf66f9aa425e8c177ac302f1d8636257c
laptopapik <nafis.iman46@gmail.com>
2017-12-05 21:59:47 +0700

    first add

---
+++ cache/_var_www_html_kelas_mzopal_phalcon-magelang_app_views_user_index.volt.php
@@ -0,0 +1,116 @@
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <h1>
+      Data Tables
+      <small>advanced tables</small>
+    </h1>
+    <ol class="breadcrumb">
+      <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+      <li><a href="#">Tables</a></li>
+      <li class="active">Data tables</li>
+    </ol>
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+    <button type="button" class="btn btn-default pull-right" data-toggle="modal" data-target="#modal-default">Add User
+    </button>
+
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="box">
+          <div class="box-header">
+            <h3 class="box-title">Data Table With Full Features</h3>
+          </div>
+          <!-- /.box-header -->
+          <div class="box-body">
+            <table id="example1" class="table table-bordered table-striped">
+              <thead>
+                <tr>
+                  <th>No.</th>
+                  <th>User Name</th>
+                  <th>Password</th>
+                  <th>Type</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php $no=1 ?> <?php foreach ($data_user as $user) { ?>
+                <tr>
+                  <td><?php echo $no++; ?></td>
+                  <td><?= $user->username ?></td>
+                  <td><?= $user->password ?></td>
+                  <td><?= $user->type ?></td>
+                  <td>
+                    <button type="button" class="btn btn-warning">Edit</button>
+                    <button type="button" class="btn btn-danger">Delete</button>
+                  </td>
+                </tr>
+                <?php } ?>
+              </tbody>
+            </table>
+          </div>
+          <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+      </div>
+      <!-- /.col -->
+    </div>
+    <!-- /.row -->
+  </section>
+  <!-- /.content -->
+  <div class="modal fade" id="modal-default">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <form class="addUser" action="user/addUser" method="POST">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title">Tambah User</h4>
+            </div>
+            <div class="modal-body">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                <input type="Text" class="form-control" placeholder="Username" name="username">
+              </div>
+              <br>
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-key"></i></span>
+                <input type="password" class="form-control" placeholder="Password" name="password">
+              </div>
+              <br>
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-book"></i></span>
+                <input type="text" class="form-control" placeholder="Type" name="type">
+              </div>
+            </div>
+            <br>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary" onclick="return addUser();">Add User</button>
+            </div>
+          </form>
+        </div>
+        <!-- /.modal-content -->
+      </div>
+      <!-- /.modal-dialog -->
+    </div>
+
+  <script>
+  function addUser(params) {
+    $.ajax({
+      method: "POST",
+      dataType: "json",
+      url: "<?= $this->url->get('user/addUser') ?>", //tujuan data  func:addUser
+      data: $('form.addUser').serialize(), //seri.. ubah jd format serialize
+      succes: function(res){
+        new PNotify({
+          title: res.title,
+          text: res.text,
+          type: res.type,
+        });
+      }
+    })
+  }
+  </script>
+
